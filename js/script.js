0a8df621d685baad560bce98c4ca24784fc37085
Kyle <khughessean01@gmail.com>
2023-07-28 13:06:05 +0200

    finetune navbar yaxis scroll

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const navBar = document.querySelector('.header__nav');
 // Event listeners
 document.addEventListener('scroll', () => {
   var scroll_position = window.scrollY;
-  if (scroll_position > 250) {
+  if (scroll_position > 100) {
     navBar.style.backgroundColor = '#175D60';
     navBar.style.boxShadow = '2px 2px 10px rgba(0, 0, 0, 0.5)';
   } else {
