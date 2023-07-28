a2a60f5bf6c60b77940c679a5c46142f91aca28b
Kyle <khughessean01@gmail.com>
2023-07-28 16:43:31 +0200

    skills grid layout

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 // Selectors
 const navBar = document.querySelector('.header__nav');
+const classList = [
+  '.main__skills',
+  '.main__project',
+  '.main__about',
+  '.main__contact',
+];
 
 // Event listeners
 document.addEventListener('scroll', () => {
@@ -11,4 +17,20 @@ document.addEventListener('scroll', () => {
     navBar.style.backgroundColor = 'transparent';
     navBar.style.boxShadow = 'none';
   }
+});
+
+classList.forEach((className) => {
+  const h2Elements = document.querySelectorAll(`${className} h2`);
+
+  h2Elements.forEach((h2) => {
+    const words = h2.textContent.split(' ');
+    h2.innerHTML = words
+      .map((word, index) => {
+        if (index % 2 !== 0) {
+          return `<span class="purple-word">${word}</span>`;
+        }
+        return word;
+      })
+      .join(' ');
+  });
 });
